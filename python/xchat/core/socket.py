50c8b47d81948b0ae5ed8a196c890ec0b70594e6
权成文 <quanchengwen@zhizhangyi.com>
2021-01-25 14:47:16 +0800

    server and client

--- python/xchat/core/socket.py
+++ python/xchat/core/socket.py
@@ -1,2 +1,32 @@
 import asyncio
+import websockets
 
+async def hello_server(websocket, path):
+    name = await websocket.recv()
+    print(f"< {name}")
+
+    greeting = f"Hello {name}!"
+
+    await websocket.send(greeting)
+    print(f"> {greeting}")
+
+def start_server():
+    loop = asyncio.new_event_loop()
+    s_server = websockets.serve(hello_server, "localhost", 8765, loop=loop)
+    loop.run_until_complete(s_server)
+    loop.run_forever()
+
+async def hello_client():
+    uri = "ws://localhost:8765"
+    async with websockets.connect(uri) as websocket:
+        name = input("Whats your name? ")
+
+        await websocket.send(name)
+        print(f"> {name}")
+
+        greeting = await websocket.recv()
+        print(f"< {greeting}")
+
+def start_client():
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(hello_client())
